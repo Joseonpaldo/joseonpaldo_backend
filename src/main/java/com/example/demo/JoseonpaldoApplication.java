e51039cd77a025c422340dcf6426854cfa78213d
themerous <lhs981130@gmail.com>
2024-07-29 17:53:13 +0900

    will it work now

--- src/main/java/com/example/demo/JoseonpaldoApplication.java
+++ src/main/java/com/example/demo/JoseonpaldoApplication.java
@@ -2,13 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class JoseonpaldoApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(JoseonpaldoApplication.class, args);
 	}
 }
