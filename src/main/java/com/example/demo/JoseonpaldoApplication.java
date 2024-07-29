0285d65d7ee2a83f70d1ea5d7ed9a47414b356f9
themerous <lhs981130@gmail.com>
2024-07-29 17:50:33 +0900

    please work for me bro

--- src/main/java/com/example/demo/JoseonpaldoApplication.java
+++ src/main/java/com/example/demo/JoseonpaldoApplication.java
@@ -9,9 +9,6 @@ public class JoseonpaldoApplication {
 
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 		SpringApplication.run(JoseonpaldoApplication.class, args);
 	}
 }
