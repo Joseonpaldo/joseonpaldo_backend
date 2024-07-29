9f566d2f366921003759db02b26d30da8b3d26e6
themerous <lhs981130@gmail.com>
2024-07-29 10:00:52 +0900

    feat: setting added for build on local

--- src/main/java/com/example/demo/JoseonpaldoApplication.java
+++ src/main/java/com/example/demo/JoseonpaldoApplication.java
@@ -2,11 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class JoseonpaldoApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 		SpringApplication.run(JoseonpaldoApplication.class, args);
 	}
 
