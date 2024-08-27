e56aee8883aff22abf9c71d78e3770e149f0c3d4
marx3042 <marx3042@gmail.com>
2024-08-27 18:29:28 +0900

    0827 18:29 authorization clear

---
+++ src/main/java/com/example/demo/data/contoller/HealthCheckController.java
@@ -0,0 +1,18 @@
+package com.example.demo.data.contoller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "ok";
+    }
+
+}
