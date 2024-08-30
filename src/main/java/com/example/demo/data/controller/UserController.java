53a80a9e933d399cf35127e394c7cd1f500c8f07
themerous <lhs981130@gmail.com>
2024-08-30 17:07:18 +0900

    added user get method

---
+++ src/main/java/com/example/demo/data/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.demo.data.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.data.entity.UserEntity;
+import com.example.demo.data.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/user")
+    public UserEntity getUser(@RequestParam("user_id") Long user_id) {
+        return userService.getUser(user_id);
+    }
+    
+}
